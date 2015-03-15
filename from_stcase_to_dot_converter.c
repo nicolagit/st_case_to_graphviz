7445b4449d93fd2d7725e4547f5cf2dcd2eab269
Nicola <nyc.zyp@gmail.com>
2015-03-15 22:48:39 +0100

    TestingCode
    Fetch last char(s) from a string

--- from_stcase_to_dot_converter.c
+++ from_stcase_to_dot_converter.c
@@ -4,14 +4,17 @@
 #include <stdbool.h>
 
 int main(int argc, char *argv[]) {
-	FILE *in_file;
-	FILE *out_file;
-	char string[100];
+	FILE *in_file, *out_file;
+	char string[100], last[2];
+	int i;
 
 	in_file=fopen(argv[1], "r");
 	out_file=fopen("output.txt", "w");
 	while (fscanf(in_file, "%s", &string) == 1) {
-		fprintf(out_file, string);
+		for (i=0; i<2; i++) {
+			last[i] = string[strlen(string) - (1-i)];
+		}
+		fprintf(out_file, last);
 		fprintf(out_file, "\n");
 	}
 }
