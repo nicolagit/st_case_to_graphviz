c214b0293a16cf7d5951a6631b6a488297f4efc0
Nicola <nyc.zyp@gmail.com>
2015-03-15 22:51:49 +0100

    TestingCode
    - Fetch file into linked list

--- from_stcase_to_dot_converter.c
+++ from_stcase_to_dot_converter.c
@@ -3,24 +3,47 @@
 #include <string.h>
 #include <stdbool.h>
 
-int main(int argc, char *argv[]) {
-	FILE *in_file, *out_file;
-	char string[100];
-	int posread=0, poswrite=0, length=10000;
-	struct word {
-		int pos;
-		char word[100];
-	};
-	struct word words[length];
-		
-	in_file=fopen(argv[1], "r");
-	out_file=fopen("output.txt", "w");
-	while (fscanf(in_file, "%s", &string) == 1) {
+
+typedef struct word {
+	int pos;
+	char *text;
+	struct word *next;
+} word;
+word* create(int pos, char *name) {
+	word *w = malloc(sizeof(word));
+	w->pos = pos;
+	w->text = strdup(name);
+	return w;
+}
+
+
+void printlist(word *start) {
+	word *w= start;
+	for (; w != NULL ; w = w->next) {
+		printf("Pos: %i, Word: %s\n", w->pos, w->text);
+		printf("Size: %i\n", strlen(w->text));
+	}
+}
+
+void readfile(FILE *in_file){
+	word *start = NULL;
+	word *w = NULL;
+	word *next = NULL;
+	char string[150];
+	int posread=1;
+	for (; fscanf(in_file, "%s", &string) == 1; w=next) {
+		next = create(posread, string);
 		posread = posread + 1;
-		words[posread].pos = posread;
-		strcpy(words[posread].word, string);
+		if (start == NULL)
+			start = next;
+		if (w != NULL)
+			w->next = next;
 	}
-	for(poswrite=0; poswrite<posread; poswrite++) {
-		fprintf(out_file, "%i, %s\n", words[poswrite+1].pos, words[poswrite+1].word);
-	}
+	printlist(start); //replace this function with the scraper
+}
+
+int main(int argc, char *argv[]) {
+	FILE *in_file;
+	in_file=fopen(argv[1], "r");
+	readfile(in_file);
 }
