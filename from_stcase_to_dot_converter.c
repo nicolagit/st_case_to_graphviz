f244c786c76a701b6a05876f21bc039f09736647
Nicola <nyc.zyp@gmail.com>
2015-03-16 13:49:48 +0100

    TestingCode
    - Add the previous word in the linkedlist

--- from_stcase_to_dot_converter.c
+++ from_stcase_to_dot_converter.c
@@ -7,32 +7,37 @@
 typedef struct word {
 	int pos;
 	char *text;
+	struct word *prev;
 	struct word *next;
 } word;
-word* create(int pos, char *name) {
+
+word* create(int pos, char *text, word *prev) {
 	word *w = malloc(sizeof(word));
 	w->pos = pos;
-	w->text = strdup(name);
+	w->text = strdup(text);
+	w->prev = prev;
 	return w;
 }
-
 
 void printlist(word *start) {
 	word *w= start;
 	for (; w != NULL ; w = w->next) {
 		printf("Pos: %i, Word: %s\n", w->pos, w->text);
 		printf("Size: %i\n", strlen(w->text));
+		printf("Pointer prev: %i", w->prev);
+		printf("Pointer next: %i\n\n", w->next);
 	}
 }
 
 void readfile(FILE *in_file){
 	word *start = NULL;
 	word *w = NULL;
+	word *prev = NULL;
 	word *next = NULL;
 	char string[150];
 	int posread=1;
 	for (; fscanf(in_file, "%s", &string) == 1; w=next) {
-		next = create(posread, string);
+		next = create(posread, string, w);
 		posread = posread + 1;
 		if (start == NULL)
 			start = next;
