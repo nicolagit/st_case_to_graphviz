02f817465b6bb69fc8e033eebec2e374b151ba9c
Nicola <nyc.zyp@gmail.com>
2015-03-16 13:49:48 +0100

    Change the way to parse the sourcefile, now a linked list is created with all words from the sourcefile

--- from_stcase_to_dot_converter.c
+++ from_stcase_to_dot_converter.c
@@ -3,62 +3,42 @@
 #include <string.h>
 #include <stdbool.h>
 
-char *output_file_name = "output.dot";
-struct transition {
-	int number;
-	char *start;
-	char *end;
-	char *comment;
-};
 
+FILE *in_file;
 
-char* find_string_between(char *left, char *right, FILE *file){
-	char string[100];
-	char string0[100];
-	char string1[100];
-	char string2[100];
-	char *graph_name = malloc(sizeof(string));
-	while (fscanf(file, "%s", &string) == 1) {
-		strcpy(string2, string1);
-		strcpy(string1, string0);
-		strcpy(string0, string);
-		if (strcmp(string2,left)==0 && (strcmp(string0,right)==0)) {
-			strcpy(graph_name, string1);
-			break;
-		}
-	}
-	return graph_name;
+typedef struct word {
+	int pos;
+	char *text;
+	struct word *prev;
+	struct word *next;
+} word;
+
+word* create(int pos, char *text, word *prev) {
+	word *w = malloc(sizeof(word));
+	w->pos = pos;
+	w->text = strdup(text);
+	w->prev = prev;
+	return w;
 }
 
-
-char* find_nodes(FILE *file) {
-	char string[100]; last[2];
-	struct node {
-		char *name;
-		struct node *next;
-	};
-	while (fscanf(file, "%s", &string) ==1) {
-		//Fetch last char of each word
-		for (i=0; i<2; i++) {
-			last[i] = string[strlen(string)-(1-i)];
-		}
-		if (strcmp(last, ":") == 0) {
-			strcpy(node, string);
+void readfile(FILE *in_file){
+	word *start = NULL;
+	word *w = NULL;
+	word *prev = NULL;
+	word *next = NULL;
+	char string[150];
+	int posread=1;
+	for (; fscanf(in_file, "%s", &string) == 1; w=next) {
+		next = create(posread, string, w);
+		posread = posread + 1;
+		if (start == NULL)
+			start = next;
+		if (w != NULL)
+			w->next = next;
 	}
-
-	char *nodes_list = malloc(sizeof(node));
 }
-
-
-//void print_output_file(){
-//	printf(out_file, "text");
-//}
-
 
 int main(int argc, char *argv[]) {
-	FILE *in_file;
-	FILE *out_file;
-	char *graph_name;
 
 	// Open the input file (with sanity check)
 	if (argc != 2) {
@@ -71,19 +51,9 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	// Fetch the name of the graph from the case's statement
-	graph_name = find_string_between("CASE", "OF", in_file);
-
-	// Generates a list of nodes from the format "NAME:"
-	node = find_nodes(in_file);
-
-	// Create the output file
-	out_file = fopen(output_file_name, "w");
-	fprintf(out_file,graph_name);
+	// Create a linked list from the file contect
+	readfile(in_file);	
 
 	// Close files and free up memory
 	fclose(in_file);
-	fclose(out_file);
-	free(graph_name);
-
 }
