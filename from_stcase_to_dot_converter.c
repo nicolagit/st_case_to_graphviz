ba9870f44004b35691c3c20a1fedcbe6d218c309
Nicola <nyc.zyp@gmail.com>
2015-03-20 10:39:16 +0100

    initial draft for state machine

--- from_stcase_to_dot_converter.c
+++ from_stcase_to_dot_converter.c
@@ -11,7 +11,6 @@ typedef struct word {
 	char *text;
 	struct word *prev, *next;
 } word;
-
 /**
  * Create a new word from the original "word" structure
  */
@@ -22,7 +21,6 @@ word* create(int pos, char *text, word *prev) {
 	w->prev = prev;
 	return w;
 }
-
 /**
  * Create a linked list from the file content
  */
@@ -42,6 +40,98 @@ word* readfile(FILE *in_file){
 }
 
 /**
+ * States for state machine and initialization
+ */
+typedef enum {
+	s0_START=0,
+	s1_Fetch_Next_Word=1,
+	s2_Found_TITLE=2,
+	s3_Found_NODE=3,
+	s4_Found_TRANSITION=4,
+	s5_Print_TRANSITION=5,
+	s6_Fault=6,
+	s7_EXIT=7,
+	sm_END=8,
+}StateType;
+StateType StateMachineActual = s0_START;
+/**
+ * Structure for the state machine
+ */
+typedef struct {
+	StateType State;
+	void (*func)(void);
+}StateMachineType;
+/**
+ * Prototype functions for STATES-FUNCTIONS table
+*/
+void Fun_s0_START(void);
+void Fun_s1_Fetch_Next_Word(void);
+void Fun_s2_Found_TITLE(void);
+void Fun_s3_Found_NODE(void);
+void Fun_s4_Found_TRANSITION(void);
+void Fun_s5_Print_TRANSITION(void);
+void Fun_s6_Fault(void);
+void Fun_s7_EXIT(void);
+/**
+ * Table with STATES-FUNCTIONS for the state machine
+*/
+StateMachineType StateMachine[] = {
+	{ s0_START,		Fun_s0_START },
+	{ s1_Fetch_Next_Word,	Fun_s1_Fetch_Next_Word },
+	{ s2_Found_TITLE,	Fun_s2_Found_TITLE },
+	{ s3_Found_NODE,	Fun_s3_Found_NODE },
+	{ s4_Found_TRANSITION,	Fun_s4_Found_TRANSITION },
+	{ s5_Print_TRANSITION,	Fun_s5_Print_TRANSITION },
+	{ s6_Fault,		Fun_s6_Fault },
+	{ s7_EXIT,		Fun_s7_EXIT }
+};
+/**
+ * Functions for the state machine
+ */
+void Fun_s0_START(void) {
+	//ADD THE CODE HERE!!!
+	//StateMachineActual = s0_START;
+}
+void Fun_s1_Fetch_Next_Word(void) {
+	//ADD THE CODE HERE!!!
+	//StateMachineActual = 
+}
+void Fun_s2_Found_TITLE(void) {
+	//ADD THE CODE HERE!!!
+	//StateMachineActual = 
+}
+void Fun_s3_Found_NODE(void) {
+	//ADD THE CODE HERE!!!
+	//StateMachineActual = 
+}
+void Fun_s4_Found_TRANSITION(void) {
+	//ADD THE CODE HERE!!!
+	//StateMachineActual = 
+}
+void Fun_s5_Print_TRANSITION(void) {
+	//ADD THE CODE HERE!!!
+	//StateMachineActual = 
+}
+void Fun_s6_Fault(void) {
+	//ADD THE CODE HERE!!!
+	//StateMachineActual = 
+}
+void Fun_s7_EXIT(void) {
+	//ADD THE CODE HERE!!!
+	//StateMachineActual = 
+}
+/**
+ * Run the state machine
+ */
+void RunStateMachine(void) {
+	if(StateMachineActual < sm_END) {
+		(*StateMachine[StateMachineActual].func)();
+	} else {
+		// Fault StateMachine out of range
+	}
+}
+
+/**
  * M A I N
  */
 int main(int argc, char *argv[]) {
@@ -57,7 +147,6 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Can't open the file\n");
 		return 1;
 	}
-
 	// Create a linked list from the file content and close the file
 	startword = readfile(in_file);	
 	fclose(in_file);
